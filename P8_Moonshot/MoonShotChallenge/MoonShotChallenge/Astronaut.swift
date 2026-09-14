64b3fcf8395f63085ce94539c9c4431108100f7f
Rahul Raoniar <rahul.raoniar@gmail.com>
2026-09-14 11:44:40 +0530

    all

---
+++ P8_Moonshot/MoonShotChallenge/MoonShotChallenge/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Rahul Raoniar on 09/09/2026.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ P8_Moonshot/MoonShotChallenge/MoonShotChallenge/MissionView.swift
@@ -0,0 +1,74 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Rahul Raoniar on 10/09/2026.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                Text(mission.formattedlaunchDate)
+                    .font(.title3)
+                    .foregroundStyle(.white.opacity(0.7))
+                    .padding(.top, 8)
+                    .padding(.bottom, 16)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+                
+                CrewView(crew: crew)
+                
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.diaplayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing astronaut data: \(member.name)")
+            }
+        }
+    }
+}
+
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return MissionView(mission: missions[1], astronauts: astronauts)
+        .preferredColorScheme(.dark)
+}
