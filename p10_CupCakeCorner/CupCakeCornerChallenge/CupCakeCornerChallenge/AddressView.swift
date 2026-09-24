f2a2ba6f86d6a9987767b4de0d03eced27bcac83
Rahul Raoniar <rahul.raoniar@gmail.com>
2026-09-24 17:57:01 +0530

    Project 9 and 10

---
+++ p10_CupCakeCorner/CupCakeCornerChallenge/CupCakeCornerChallenge/AddressView.swift
@@ -0,0 +1,36 @@
+//
+//  AddressView.swift
+//  BasicOrder
+//
+//  Created by Rahul Raoniar on 22/09/2026.
+
+import SwiftUI
+
+struct AddressView: View {
+    
+    @Bindable var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ p10_CupCakeCorner/CupCakeCornerChallenge/CupCakeCornerChallenge/CheckoutView.swift
@@ -0,0 +1,85 @@
+//
+//  CheckoutView.swift
+//  Day51CupCakeCorner
+//
+//  Created by Rahul Raoniar on 23/09/2026.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 223)
+                
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+        .alert("Order failed", isPresented: $showingError) {
+            Button("Ok") { }
+        } message: {
+            Text(errorMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("USE API KEY HERE", forHTTPHeaderField: "x-api-key")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            errorMessage = error.localizedDescription
+            showingError = true
+        }
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
